657229841e7fd4c4ee40e3c268c961e15b82a46f
Emerson Andy <emersonbataguassu@gmail.com>
2026-06-14 11:57:05 -0300

    First Commit

---
+++ src/main/java/dev/andy/CrudCadastro/CrudCadastroApplication.java
@@ -0,0 +1,13 @@
+package dev.andy.CrudCadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/andy/CrudCadastro/CrudCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andy.CrudCadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
